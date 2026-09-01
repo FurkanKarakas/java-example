96196bd923b9679822ed4d6a23deb656439977d1
Furkan Karakas <fkarakasster@gmail.com>
2026-09-01 23:59:55 +0200

    Initial commit.

---
+++ demo/src/test/java/com/example/furkankarakas/AppTest.java
@@ -0,0 +1,23 @@
+package com.example.furkankarakas;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ * Writing tests in JUnit: https://docs.junit.org/6.1.3/writing-tests/intro.html
+ * AssertJ: https://assertj.github.io/doc/
+ * Selenium docs: https://www.selenium.dev/documentation/
+ * Selenium PageFactory:
+ * https://www.selenium.dev/selenium/docs/api/java/org/openqa/selenium/support/PageFactory.html
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
