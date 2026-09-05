0c3eccb53b9257711b0ecbe0182468e727bbc1f5
Furkan Karakas <fkarakasster@gmail.com>
2026-09-05 23:34:11 +0200

    Added AssertJ library and more tests.

--- demo/src/test/java/com/example/furkankarakas/AppTest.java
+++ demo/src/test/java/com/example/furkankarakas/AppTest.java
@@ -2,8 +2,10 @@ package com.example.furkankarakas;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assumptions.*;
+
+import java.math.BigDecimal;
 
 /**
  * Unit test for simple App.
@@ -23,7 +25,7 @@ public class AppTest {
         String input = "hello";
         String expected = "olleh";
         String actual = StringUtils.reverse(input);
-        assertTrue(expected.equals(actual));
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
@@ -31,14 +33,29 @@ public class AppTest {
         String input = null;
         String expected = "";
         String actual = StringUtils.reverse(input);
-        assertTrue(expected.equals(actual));
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     public void givenString_whenCharacterArrayConverted_thenCorrect() {
         String input = "hello";
+        assumeThat(input).isNotEmpty();
+
         Character[] expected = new Character[] { 'h', 'e', 'l', 'l', 'o' };
         Character[] actual = StringUtils.toCharacterArray(input);
-        assertArrayEquals(expected, actual);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void givenTwoBigDecimals_whenAdded_thenCorrect() {
+        BigDecimal bd1 = BigDecimal.valueOf(0.1);
+        BigDecimal bd2 = BigDecimal.valueOf(0.1);
+        BigDecimal expected = new BigDecimal("0.200");
+        BigDecimal actual = bd1.add(bd2);
+        // Compares the big decimal by using the compareTo method from the Comparable
+        // interface. Note that isEqualTo would have failed the test since the scales of
+        // the numbers are different.
+        assertThat(actual).isEqualByComparingTo(expected);
     }
 }
